a903083b99074d931d600b28bace15aa9e9d5b98
ewenyau123 <52481528+ewenyau123@users.noreply.github.com>
2024-04-24 19:13:07 +0800

    Update pxt.json, main.blocks, main.ts, README.md

---
+++ main.ts
@@ -0,0 +1,272 @@
+namespace IFS_IOT {
+    enum Cmd {
+        None,
+        ConnectWifi,
+        ConnectMqtt,
+    }
+
+
+    export enum SchemeList {
+        //% block="TCP"
+        TCP = 1,
+        //% block="TLS"
+        TLS = 2
+    }
+
+    export enum QosList {
+        //% block="0"
+        Qos0 = 0,
+        //% block="1"
+        Qos1,
+        //% block="2"
+        Qos2
+    }
+
+    let wifi_connected: boolean = false
+    let mqttBrokerConnected: boolean = false
+    let userToken_def: string = ""
+    let topic_def: string = ""
+    const mqttSubscribeHandlers: { [topic: string]: (message: string) => void } = {}
+    const mqttSubscribeQos: { [topic: string]: number } = {}
+    let mqtthost_def = "IFS"
+
+    let serialCnt = 0
+    let recvString = ""
+    let scanWIFIAPFlag = 0
+    let currentCmd: Cmd = Cmd.None
+
+    const EspEventSource = 3000
+    const EspEventValue = {
+        None: Cmd.None,
+        ConnectWifi: Cmd.ConnectWifi,
+        ConnectMqtt: Cmd.ConnectMqtt,
+    }
+
+    let TStoSendStr = ""
+
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 0) {
+        serial.writeString(`${command}\u000D\u000A`)
+        basic.pause(wait)
+    }
+
+    function restEsp8266() {
+        sendAT("AT+RESTORE", 1000) // restore to factory settings
+        sendAT("AT+RST", 1000) // rest
+        serial.readString()
+        sendAT("AT+CWMODE=1", 500) // set to STA mode
+        sendAT("AT+SYSTIMESTAMP=1634953609130", 100) // Set local timestamp.
+        sendAT(`AT+CIPSNTPCFG=1,8,"ntp1.aliyun.com","0.pool.ntp.org","time.google.com"`, 100)
+        basic.pause(3000)
+    }
+
+    function scanWIFIAP(ssid: string) {
+
+        let scanflag = 0
+        let mscnt = 0
+        recvString = " "
+        sendAT(`AT+CWLAPOPT=1,2,-100,255`)
+        sendAT(`AT+CWLAP`)
+        while (!(scanflag)) {
+
+            recvString = recvString + serial.readString()
+            basic.pause(1)
+            mscnt += 1
+            if (mscnt >= 3000) {
+                scanWIFIAPFlag = 0
+                break
+            }
+
+            if (recvString.includes("+CWLAP:(")) {
+
+                mscnt = 0
+                recvString = recvString.slice(recvString.indexOf("+CWLAP:("))
+                scanflag = 1
+                while (1) {
+
+                    recvString += serial.readString()
+                    basic.pause(1)
+                    mscnt += 1
+
+                    // OLED.clear()
+                    // OLED.writeStringNewLine(_recvString)
+                    if (recvString.includes("OK") || mscnt >= 3000) {
+
+                        if (mscnt >= 3000) {
+                            scanWIFIAPFlag = 0
+                        } else if (recvString.includes(ssid)) {
+                            scanWIFIAPFlag = 1
+                        } else {
+                            scanWIFIAPFlag = 0
+                        }
+                        break
+                    }
+                }
+            }
+
+        }
+        recvString = " "
+    }
+
+
+    /**
+     * connect to Wifi router
+     */
+    //% block="connect SmartClassroom Wifi Name = %ssid|Password = %pw"
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_pwd
+    export function connectWifi(ssid: string, pw: string) {
+        serial.redirect(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+        basic.pause(100)
+        serial.setTxBufferSize(128)
+        serial.setRxBufferSize(128)
+        restEsp8266()
+        while (1) {
+            scanWIFIAP(ssid)
+            if (scanWIFIAPFlag) {
+                currentCmd = Cmd.ConnectWifi
+                sendAT(`AT+CWJAP="${ssid}","${pw}"`) // connect to Wifi router
+                control.waitForEvent(EspEventSource, EspEventValue.ConnectWifi)
+                while (!wifi_connected) {
+                    restEsp8266()
+                    sendAT(`AT+CWJAP="${ssid}","${pw}"`)
+                    control.waitForEvent(EspEventSource, EspEventValue.ConnectWifi)
+                }
+                break
+            } else {
+                restEsp8266()
+                currentCmd = Cmd.ConnectWifi
+                sendAT(`AT+CWJAP="${ssid}","${pw}"`)
+                control.waitForEvent(EspEventSource, EspEventValue.ConnectWifi)
+                if (wifi_connected) {
+                    break
+                }
+            }
+        }
+    }
+
+    /**
+     * Warning: Deprecated.
+     * Check if ESP8266 successfully connected to Wifi
+     */
+    //% block="Wifi connected %State" weight=70
+    export function wifiState(state: boolean) {
+        return wifi_connected === state
+    }
+
+    /*----------------------------------MQTT-----------------------*/
+    /*
+     * Set  MQTT client
+     */
+    //% subcategory=MQTT weight=30
+    //% blockId=initMQTT block="Set MQTT connection config|scheme: %scheme clientID: %clientID username: %username password: %password"
+    export function setMQTT(scheme: SchemeList, clientID: string, username: string, password: string): void {
+        sendAT(`AT+MQTTUSERCFG=0,${scheme},"${clientID}","${username}","${password}",0,0,""`, 1000)
+    }
+
+    /*
+     * Connect to MQTT broker
+     */
+    //% subcategory=MQTT weight=25
+    //% blockId=connectMQTT block="connect SmartClassroom Server IP Address: %host port: %port reconnect: $reconnect"
+    export function connectMQTT(host: string, port: number, reconnect: boolean): void {
+        mqtthost_def = host
+        const rec = reconnect ? 0 : 1
+        currentCmd = Cmd.ConnectMqtt
+        sendAT(`AT+MQTTCONN=0,"${host}",${port},${rec}`)
+        control.waitForEvent(EspEventSource, EspEventValue.ConnectMqtt)
+        Object.keys(mqttSubscribeQos).forEach(topic => {
+            const qos = mqttSubscribeQos[topic]
+            sendAT(`AT+MQTTSUB=0,"${topic}",${qos}`, 1000)
+        })
+    }
+
+    /*
+     * Check if ESP8266 successfully connected to mqtt broker
+     */
+    //% block="Smart Classroom Hub is connected"
+    //% subcategory="MQTT" weight=24
+    export function isMqttBrokerConnected() {
+        return mqttBrokerConnected
+    }
+
+    /*
+     * send message
+     */
+    //% subcategory=MQTT weight=21
+    //% blockId=sendMQTT block="publish %msg to Topic:%topic with Qos:%qos"
+    //% msg.defl=message
+    //% topic.defl=smartclassroom/demo
+    export function publishMqttMessage(msg: string, topic: string, qos: QosList): void {
+        sendAT(`AT+MQTTPUB=0,"${topic}","${msg}",${qos},0`, 1000)
+        recvString = ""
+    }
+
+    /*
+     * disconnect MQTT broker
+     */
+    //% subcategory=MQTT weight=15
+    //% blockId=breakMQTT block="Disconnect from Smart Classroom Hub"
+    export function breakMQTT(): void {
+        sendAT("AT+MQTTCLEAN=0", 1000)
+    }
+
+    //% block="when Topic: %topic have new $message with Qos: %qos"
+    //% subcategory=MQTT weight=10
+    //% draggableParameters
+    //% topic.defl=smartclassroom/demo
+    export function MqttEvent(topic: string, qos: QosList, handler: (message: string) => void) {
+        mqttSubscribeHandlers[topic] = handler
+        mqttSubscribeQos[topic] = qos
+    }
+
+
+    /*
+     * on serial received data
+     */
+    serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+        recvString += serial.readString()
+        pause(1)
+        serialCnt += 1
+        if (recvString.includes("MQTTSUBRECV")) {
+            recvString = recvString.slice(recvString.indexOf("MQTTSUBRECV"))
+            const recvStringSplit = recvString.split(",", 4)
+            const topic = recvStringSplit[1].slice(1, -1)
+            const message = recvStringSplit[3].slice(0, -2)
+            mqttSubscribeHandlers[topic] && mqttSubscribeHandlers[topic](message)
+            recvString = ""
+        }
+
+
+        switch (currentCmd) {
+            case Cmd.ConnectWifi:
+                if (recvString.includes("AT+CWJAP")) {
+                    recvString = recvString.slice(recvString.indexOf("AT+CWJAP"))
+                    if (recvString.includes("WIFI GOT IP")) {
+                        wifi_connected = true
+                        recvString = ""
+                        control.raiseEvent(EspEventSource, EspEventValue.ConnectWifi)
+                    } else if (recvString.includes("ERROR")) {
+                        wifi_connected = false
+                        recvString = ""
+                        control.raiseEvent(EspEventSource, EspEventValue.ConnectWifi)
+                    }
+                }
+                break
+            case Cmd.ConnectMqtt:
+                if (recvString.includes(mqtthost_def)) {
+                    recvString = recvString.slice(recvString.indexOf(mqtthost_def))
+                    if (recvString.includes("OK")) {
+                        mqttBrokerConnected = true
+                        recvString = ""
+                        control.raiseEvent(EspEventSource, EspEventValue.ConnectMqtt)
+                    } else if (recvString.includes("ERROR")) {
+                        mqttBrokerConnected = false
+                        recvString = ""
+                        control.raiseEvent(EspEventSource, EspEventValue.ConnectMqtt)
+                    }
+                }
+                break
+        }
+    })
+}
